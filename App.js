cda7cb8b2b1fafeb41c363b7ec9b9b543154337f
cabzmo <cab_98@icloud.com>
2021-12-23 18:00:49 +0000

    played with image and touchable highlights

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text clicked");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello React Native - A really really long text. I wanna make this even
-        longer and see what happens
+      <Text onPress={() => console.log("text clicked")}>
+        Hello React Native
       </Text>
+      <TouchableHighlight onPress={() => console.log("remote image clicked")}>
+        <Image
+          blurRadius={3}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => console.log("local image clicked")}>
+        <Image source={require("./assets/favicon.png")} />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -18,6 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
