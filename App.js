1cf05d7b88aff1ee4dfd8979d835e98859bd28fe
cabzmo <cab_98@icloud.com>
2021-12-24 14:35:05 +0000

    @react-native-community/hooks detect orientation made video player: full width and height in landscape 30% height in portrait

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import {
+  Dimensions,
   StyleSheet,
   Text,
-  View,
+  Platform,
   Image,
   SafeAreaView,
+  View,
   TouchableHighlight,
+  Button,
 } from "react-native";
+import {
+  useDeviceOrientation,
+  useDimensions,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={() => console.log("text clicked")}>
-        Hello React Native
-      </Text>
-      <TouchableHighlight onPress={() => console.log("remote image clicked")}>
-        <Image
-          blurRadius={3}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => console.log("local image clicked")}>
-        <Image source={require("./assets/favicon.png")} />
-      </TouchableHighlight>
       <StatusBar style="auto" />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+        }}
+      />
     </SafeAreaView>
   );
 }
@@ -37,7 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
