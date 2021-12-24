e31440d612d181417902da6fd90dc471e914a7eb
cabzmo <cab_98@icloud.com>
2021-12-24 19:38:38 +0000

    view image screen complete

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,26 +1,44 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView, View, Image } from "react-native";
 
 function ViewImageScreen(props) {
   return (
-    <View style={styles.background}>
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 50, height: 50 }}
-      ></View>
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 50, height: 50 }}
-      ></View>
-    </View>
+    <SafeAreaView style={styles.containter}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  closeIcon: {
+    width: 35,
+    height: 35,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  containter: {
     backgroundColor: "#000000",
+    flex: 1,
+  },
+  deleteIcon: {
+    width: 35,
+    height: 35,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  image: {
     width: "100%",
-    height: 100,
-    alignItems: "center",
-    flexDirection: "row",
+    height: "100%",
   },
 });
 
