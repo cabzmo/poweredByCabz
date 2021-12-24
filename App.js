57700bc6978e489d808ca5656195a0f26c739fc5
cabzmo <cab_98@icloud.com>
2021-12-24 15:09:41 +0000

    messed around with flexbox

--- App.js
+++ App.js
@@ -10,25 +10,58 @@ import {
   TouchableHighlight,
   Button,
 } from "react-native";
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
+import { useDimensions } from "@react-native-community/hooks";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          width: 100,
+          height: 100,
         }}
       />
-    </SafeAreaView>
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
