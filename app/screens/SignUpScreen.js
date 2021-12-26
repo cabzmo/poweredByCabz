e3ff45fbdd2ae4c34e90a97bd0ca3b60e65997cb
cabzmo <cab_98@icloud.com>
2021-12-26 16:35:31 +0000

    layout of form complete with colours

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
 
 import backgroundImage from "../config/backgroundImage";
 
@@ -9,21 +17,42 @@ function SignUpScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <View style={[{ backgroundColor: "white" }, styles.containers]}>
-        <View style={[{ backgroundColor: "dodgerblue" }, styles.boxes]}>
-          <Text>Label</Text>
-        </View>
-        <View style={[{ backgroundColor: "tomato" }, styles.boxes]} />
-        <View style={[{ backgroundColor: "gold" }, styles.boxes]} />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.logoText}>Sell What You Don't Need</Text>
       </View>
+      <View style={styles.placeholder}>
+        <View style={[styles.containers, { backgroundColor: "grey" }]}>
+          <View style={[styles.boxes, { backgroundColor: "dodgerblue" }]}>
+            <View style={styles.labelHolder}>
+              <Text style={styles.labelsText}>Username:</Text>
+            </View>
+          </View>
+          <View style={[styles.boxes, { backgroundColor: "tomato" }]}>
+            <View style={styles.labelHolder}>
+              <Text style={styles.labelsText}>Password:</Text>
+            </View>
+          </View>
+          <View style={[styles.boxes, { backgroundColor: "gold" }]}>
+            <View style={styles.labelHolder}>
+              <Text style={styles.labelsText}>Confirm Password:</Text>
+            </View>
+          </View>
+        </View>
 
-      <View style={[{ backgroundColor: "black" }, styles.containers]}>
-        <View style={[{ backgroundColor: "dodgerblue" }, styles.boxes]}>
-          <Text>TextBox</Text>
+        <View style={[{ backgroundColor: "black" }, styles.containers]}>
+          <View style={[{ backgroundColor: "dodgerblue" }, styles.boxes]}>
+            <View style={[{ backgroundColor: "white" }, styles.textbox]} />
+          </View>
+          <View style={[{ backgroundColor: "tomato" }, styles.boxes]}>
+            <View style={[{ backgroundColor: "white" }, styles.textbox]} />
+          </View>
+          <View style={[{ backgroundColor: "gold" }, styles.boxes]}>
+            <View style={[{ backgroundColor: "white" }, styles.textbox]} />
+          </View>
         </View>
-        <View style={[{ backgroundColor: "tomato" }, styles.boxes]} />
-        <View style={[{ backgroundColor: "gold" }, styles.boxes]} />
       </View>
+      <StatusBar style={"auto"} />
     </ImageBackground>
   );
 }
@@ -37,12 +66,40 @@ const styles = StyleSheet.create({
   },
   boxes: {
     width: 150,
-    height: 30,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
   },
   containers: {
     width: 150,
     height: 135,
     justifyContent: "space-evenly",
+  },
+  labelHolder: {
+    width: "90%",
+    height: "100%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  labelsText: {
+    fontWeight: "600",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  placeholder: {
+    flexDirection: "row",
+    top: 50,
+  },
+  textbox: {
+    width: "90%",
+    height: "70%",
   },
 });
 
